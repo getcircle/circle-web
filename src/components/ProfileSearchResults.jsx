5e29f5ae85aad3a00ab09ffdf71b2362f0f55aa9
Michael Hahn <mwhahn@gmail.com>
2015-07-02 11:54:09 -0700

    Remove unused import

--- src/components/ProfileSearchResults.jsx
+++ src/components/ProfileSearchResults.jsx
@@ -1,14 +1,11 @@
 'use strict';
 
-import mui from 'material-ui';
 import React from 'react';
 
 import ProfileSearchResult from './ProfileSearchResult';
 import SearchResult from './SearchResult';
 import SearchResultHeader from './SearchResultHeader';
 import SearchResultsContainer from './SearchResultsContainer';
-
-const {Avatar} = mui;
 
 class ProfileSearchResults extends React.Component {
 
