cc8378ef2961fe3f3785e56d2ba6e0a436f55232
Parris Khachi <parris@letswoosh.com>
2015-07-04 00:10:21 -0700

    Better gulp js and watching
    * Growl notifications
    * Faster watching
    * Source maps

--- tasks/watch.js
+++ tasks/watch.js
@@ -4,13 +4,7 @@ var gulp = require('gulp');
 var watch = require('gulp-watch');
 
 gulp.task('watch', function () {
-    watch(['src/**/**.jsx'], function () {
-        gulp.start('js');
-    });
-
-    watch(['src/**/**.js'], function () {
-        gulp.start('js');
-    });
+    gulp.start('js-watch');
 
     watch(['src/styles/**/**.scss'], function () {
         gulp.start('sass');
