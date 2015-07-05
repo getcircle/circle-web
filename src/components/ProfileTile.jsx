d4dffe7182f9261a8a51a3d331fb7ddbbf884839
Parris Khachi <parris@letswoosh.com>
2015-07-04 17:06:39 -0700

    Extracted style code into its own function

--- src/components/ProfileTile.jsx
+++ src/components/ProfileTile.jsx
@@ -33,35 +33,42 @@ class ProfileTile extends React.Component {
         })
     }
 
+    _getStyles() {
+        return {
+            paper: {
+                textAlign: 'center',
+                height: styleConstants.verticalRhythm * 20,
+                position: 'relative',
+                marginTop: styleConstants.verticalRhythm * 2,
+            },
+            avatar: {
+                marginTop: styleConstants.verticalRhythm,
+            },
+            footer: {
+                background: this.state.footerColor,
+                height: styleConstants.verticalRhythm * 6,
+                position: 'absolute',
+                bottom: 0,
+                width: '100%',
+
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                lineHeight: '1em',
+            },
+        };
+    }
+
+
     render() {
         const profile = this.props.profile;
-        const paperStyle = {
-            textAlign: 'center',
-            height: styleConstants.verticalRhythm * 20,
-            position: 'relative',
-            marginTop: styleConstants.verticalRhythm * 2,
-        };
-        const avatarStyle = {
-            marginTop: styleConstants.verticalRhythm,
-        };
-        const footer = {
-            background: this.state.footerColor,
-            height: styleConstants.verticalRhythm * 6,
-            position: 'absolute',
-            bottom: 0,
-            width: '100%',
-
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            lineHeight: '1em',
-        };
+        const styles = this._getStyles();
         // "Avatar" seems to be causing the page to load slowly since its fetching all the images at once. Is there a better way we can do this where the images will fade in as well?
         return (
-            <Paper style={paperStyle}>
-                <Avatar src={profile.image_url} style={avatarStyle} />
+            <Paper style={styles.paper}>
+                <Avatar src={profile.image_url} style={styles.avatar} />
                 <p>{profile.full_name}</p>
-                <footer style={footer}>{profile.title}</footer>
+                <footer style={styles.footer}>{profile.title}</footer>
             </Paper>
         );
     }
