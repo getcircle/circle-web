834183e12f6449a0964556c7b9bb70700389bfee
Ravi Rani <ravirani@gmail.com>
2015-07-06 01:29:38 -0700

    Link profile detail from profiles view

--- src/components/ProfileTile.jsx
+++ src/components/ProfileTile.jsx
@@ -1,7 +1,9 @@
 'use strict';
 
 import _ from 'lodash';
+import { decorate } from 'react-mixin';
 import mui from 'material-ui';
+import { Navigation } from 'react-router';
 import React from 'react';
 
 import styleConstants from '../styles/constants';
@@ -10,6 +12,7 @@ import ThemeManager from '../utils/ThemeManager';
 const {Avatar} = mui;
 const {Paper} = mui;
 
+@decorate(Navigation)
 class ProfileTile extends React.Component {
 
     static propTypes = {
@@ -36,12 +39,17 @@ class ProfileTile extends React.Component {
         };
     }
 
+    _handleOnClick = this._handleOnClick.bind(this) 
+    _handleOnClick(event) {
+        this.transitionTo(`/profile/${this.props.profile.id}`);
+    }
+
     render() {
         const profile = this.props.profile;
 
         // "Avatar" seems to be causing the page to load slowly since its fetching all the images at once. Is there a better way we can do this where the images will fade in as well?
         return (
-            <Paper className="profile-tile stack__item">
+            <Paper className="profile-tile stack__item" onClick={this._handleOnClick}>
                 <Avatar className="stack__item" src={profile.small_image_url || profile.image_url} style={this.styles.avatar} />
                 <p className="stack__item">{profile.full_name}</p>
                 <p className="profile-title__text--secondary">{profile.title}</p>
