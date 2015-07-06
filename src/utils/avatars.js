01832cb3532c548ed9b7ac1335d0a6cf97e26329
Michael Hahn <mwhahn@gmail.com>
2015-07-06 16:17:29 -0700

    Store alphaColors in localStorage so they persist between refreshes

--- src/utils/avatars.js
+++ src/utils/avatars.js
@@ -4,10 +4,14 @@ import _ from 'lodash';
 
 import constants from '../styles/constants';
 
-let alphaColors = {};
-
+const key = 'alphaColors';
 
 export function getColorForProfile(profile) {
+    let alphaColors = {};
+    if (window.localStorage.getItem(key)) {
+        alphaColors = JSON.parse(window.localStorage.getItem(key));
+    }
+
     let character = profile.first_name[0];
     let color = alphaColors[character];
     if (!color) {
@@ -15,5 +19,6 @@ export function getColorForProfile(profile) {
         color = colors[_.random(0, colors.length - 1)];
         alphaColors[character] = color;
     }
+    window.localStorage.setItem(key, JSON.stringify(alphaColors));
     return color;
 }
