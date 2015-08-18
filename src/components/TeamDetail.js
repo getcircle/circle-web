ceb2dd05365f0963d61830eb8846d74e933f636c
Michael Hahn <mwhahn@gmail.com>
2015-08-18 12:15:37 -0700

    Add "Teams" section to TeamDetail

--- src/components/TeamDetail.js
+++ src/components/TeamDetail.js
@@ -5,11 +5,27 @@ import React from 'react';
 import { routeToProfile, routeToTeam } from '../utils/routes';
 import t from '../utils/gettext';
 
+import Card from './Card';
+import CardList from './CardList';
+import CardListItem from './CardListItem';
+import CardRow from './CardRow';
+import CardVerticalDivider from './CardVerticalDivider';
 import DetailContent from './DetailContent';
+import ProfileAvatar from './ProfileAvatar';
 import StyleableComponent from './StyleableComponent';
 import TeamDetailHeader from './TeamDetailHeader';
+import TeamDetailTeams from './TeamDetailTeams';
 
-const styles = {};
+const styles = {
+    description: {
+        padding: 20,
+        lineHeight: '24px',
+        fontSize: '14px',
+    },
+    section: {
+        marginTop: 20,
+    },
+};
 
 @decorate(Navigation)
 class TeamDetail extends StyleableComponent {
@@ -18,12 +34,55 @@ class TeamDetail extends StyleableComponent {
         extendedTeam: React.PropTypes.object.isRequired,
     }
 
+    _renderDescription(team) {
+        // if (team.description) {
+        if (true) {
+            return (
+                <Card title="Description" style={styles.section}>
+                    <CardRow>
+                        <span style={styles.description}>
+                            Some description about the team
+                        </span>
+                    </CardRow>
+                </Card>
+            );
+        }
+    }
+
+    _renderManager(manager) {
+        return (
+            <Card title="Manager" style={styles.section}>
+                <CardRow>
+                    <CardList>
+                        <CardListItem
+                            primaryText={manager.full_name}
+                            secondaryText={manager.title}
+                            leftAvatar={<ProfileAvatar profile={manager} />}
+                            onTouchTap={routeToProfile.bind(this, manager)}
+                        />
+                    </CardList>
+                </CardRow>
+            </Card>
+        );
+    }
+
+    _renderChildTeams(childTeams) {
+        if (childTeams) {
+            return <TeamDetailTeams style={styles.section} teams={childTeams}/>;
+        }
+    }
+
     render() {
         const { extendedTeam } = this.props;
+        const { team, reportingDetails } = extendedTeam;
+        const { manager, members, childTeams } = reportingDetails;
         return (
             <div>
-                <TeamDetailHeader team={extendedTeam.team} />
+                <TeamDetailHeader team={team} />
                 <DetailContent>
+                    {this._renderDescription(team)}
+                    {this._renderManager(manager)}
+                    {this._renderChildTeams(childTeams)}
                 </DetailContent>
             </div>
         );
---
+++ src/components/TeamDetailTeams.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { services } from 'protobufs';
+
+import { getTeamLabel } from '../services/organization';
+
+import Card from './Card';
+import CardFooter from './CardFooter';
+import CardList from './CardList';
+import CardListItem from './CardListItem';
+import CardRow from './CardRow';
+import CardVerticalDivider from './CardVerticalDivider';
+import GroupIcon from './OfficeIcon';
+import IconContainer from './IconContainer';
+import StyleableComponent from './StyleableComponent';
+
+const styles = {
+    icon: {
+        color: 'rgba(0, 0, 0, .4)',
+    },
+    teamsFooterText: {
+        fontSize: '16px',
+        fontWeight: 600,
+        lineHeight: '22px',
+        alignSelf: 'center',
+        paddingLeft: 28,
+        color: 'rgba(0, 0, 0, .6)',
+    },
+};
+
+class TeamDetailTeams extends StyleableComponent {
+
+    static propTypes = {
+        teams: React.PropTypes.arrayOf(
+            React.PropTypes.instanceOf(services.organization.containers.TeamV1),
+        ).isRequired,
+    }
+
+    _renderColumn(teams) {
+        const children = teams.map((item, index) => {
+            return (
+                <CardListItem
+                    key={index}
+                    primaryText={item.name}
+                    secondaryText={getTeamLabel(item)}
+                    leftAvatar={<IconContainer IconClass={GroupIcon} stroke={styles.icon.color} />}
+                />
+            );
+        })
+        return (
+            <CardList>
+                {children}
+            </CardList>
+        );
+    }
+
+    _renderCardFooter(teams) {
+        if (teams.length > 4) {
+            return (
+                <CardFooter actionText="view all teams">
+                    <span style={styles.teamsFooterText}>{`${teams.length} teams`}</span>
+                </CardFooter>
+            );
+        }
+    }
+
+    render() {
+        const { teams } = this.props;
+        // break out teams into two columns with a max of two teams each
+        const firstColumn = teams.slice(0, 2);
+        const secondColumn = teams.slice(2, 4);
+        return (
+            <Card {...this.props} title="Teams">
+                <CardRow>
+                    {this._renderColumn(firstColumn)}
+                    <CardVerticalDivider />
+                    {this._renderColumn(secondColumn)}
+                </CardRow>
+                {this._renderCardFooter(teams)}
+            </Card>
+        );
+    }
+
+}
+
+export default TeamDetailTeams;
