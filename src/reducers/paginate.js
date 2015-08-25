44ab0f162ada057df3d8ae3b225e7036b4888ae6
Michael Hahn <mwhahn@gmail.com>
2015-08-25 12:14:30 -0700

    Add pagination reducer factory

---
+++ src/reducers/paginate.js
@@ -0,0 +1,61 @@
+import Immutable from 'immutable';
+
+// ttl interval in milliseconds
+const TTL_INTERVAL = 300000
+
+export default function paginate({ types, mapActionToKey }) {
+    if (!Array.isArray(types) || types.length !== 3) {
+        throw new Error('Expected types to be an array of three elements');
+    }
+
+    if (!types.every(t => typeof t === 'string')) {
+        throw new Error('Expected types to be strings');
+    }
+
+    if (typeof mapActionToKey !== 'function') {
+        throw new Error('Expected mapActionToKey to be a function');
+    }
+
+    const [requestType, successType, failureType] = types;
+
+    function updatePagination(state = Immutable.fromJS({
+        loading: false,
+        nextRequest: null,
+        ids: Immutable.OrderedSet(),
+        ttl: null,
+    }), action) {
+        const { payload } = action;
+        switch (action.type) {
+        case requestType:
+            return state.set('loading', true);
+        case successType:
+            return state.withMutations(map => {
+                return map.updateIn(['ids'], set => set.union(payload.result))
+                    .set('loading', false)
+                    .set('ttl', Date.now() + TTL_INTERVAL)
+                    .set('nextRequest', payload.nextRequest);
+            });
+        case failureType:
+            return state.set('loading', false);
+        default:
+            return state;
+        }
+    }
+
+    return function updatePaginationByKey(state = Immutable.Map(), action) {
+        switch(action.type) {
+        case requestType:
+        case successType:
+        case failureType:
+            const key = mapActionToKey(action);
+            return state.withMutations(map => {
+                return map.mergeDeep({
+                    [key]: updatePagination(map.get(key), action),
+                });
+            });
+        default:
+            return state;
+        }
+    };
+
+}
