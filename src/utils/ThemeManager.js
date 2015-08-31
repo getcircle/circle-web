a4d1bcb2ab80f6b3f4350a2a9d6e91152ff0c5ee
Michael Hahn <mwhahn@gmail.com>
2015-08-30 22:52:02 -0700

    Support creating a new ThemeManager
    Components can use this to customize children

--- src/utils/ThemeManager.js
+++ src/utils/ThemeManager.js
@@ -7,56 +7,61 @@ import {
     tintColor
 } from '../constants/styles';
 
-const ThemeManager = new mui.Styles.ThemeManager();
+export function getCustomThemeManager() {
+    const ThemeManager = new mui.Styles.ThemeManager();
 
-// Custom Theme
-ThemeManager.contentFontFamily = '"Open Sans", Arial, sans-serif';
-ThemeManager.setPalette({
-    accent1Color: tintColor,
-    canvasColor: canvasColor,
-});
-ThemeManager.setComponentThemes({
-    appBar: {
-        color: '#F7F9FA',
-    },
-    tab: {
-        textColor: 'rgba(0, 0, 0, 0.3)',
-        selectedTextColor: tintColor,
-    },
-    tabs: {
-        backgroundColor: 'rgba(255, 255, 255, 0.5)',
-    },
-});
+    // Custom Theme
+    ThemeManager.contentFontFamily = '"Open Sans", Arial, sans-serif';
+    ThemeManager.setPalette({
+        accent1Color: tintColor,
+        canvasColor: canvasColor,
+    });
+    ThemeManager.setComponentThemes({
+        appBar: {
+            color: '#F7F9FA',
+        },
+        tab: {
+            textColor: 'rgba(0, 0, 0, 0.3)',
+            selectedTextColor: tintColor,
+        },
+        tabs: {
+            backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        },
+    });
 
-/**
- * The purpose of this object is to consolidate any common styles
- * for things that are shared amongst various components.
- *
- * Common styles between components is RARE and if they are sharing too many of
- * these, you should ask why there is no one component representing them.
- *
- * THIS SHOULD BE KEPT VERY SMALL, SO PLEASE DO NOT ABUSE THIS AS A PLACE FOR ADDING ALL CSS.
- */
-ThemeManager.commonStyles = {
-    headerPrimaryText: {
-        fontSize: '36px',
-        lineHeight: '49px',
-        ...fontColors.white,
-        ...fontWeights.light,
-    },
-    headerSecondaryText: {
-        textTransform: 'uppercase',
-        fontSize: '12px',
-        lineHeight: '17px',
-        letterSpacing: '2px',
-        ...fontColors.white,
-        ...fontWeights.semiBold,
-    },
-    headerTertiaryText: {
-        fontSize: '12px',
-        lineHeight: '18px',
-        ...fontColors.darkWhite,
-    },
-};
+    /**
+     * The purpose of this object is to consolidate any common styles
+     * for things that are shared amongst various components.
+     *
+     * Common styles between components is RARE and if they are sharing too many of
+     * these, you should ask why there is no one component representing them.
+     *
+     * THIS SHOULD BE KEPT VERY SMALL, SO PLEASE DO NOT ABUSE THIS AS A PLACE FOR ADDING ALL CSS.
+     */
+    ThemeManager.commonStyles = {
+        headerPrimaryText: {
+            fontSize: '36px',
+            lineHeight: '49px',
+            ...fontColors.white,
+            ...fontWeights.light,
+        },
+        headerSecondaryText: {
+            textTransform: 'uppercase',
+            fontSize: '12px',
+            lineHeight: '17px',
+            letterSpacing: '2px',
+            ...fontColors.white,
+            ...fontWeights.semiBold,
+        },
+        headerTertiaryText: {
+            fontSize: '12px',
+            lineHeight: '18px',
+            ...fontColors.darkWhite,
+        },
+    };
 
-export default ThemeManager;
+    return ThemeManager;
+}
+
+export default getCustomThemeManager();
+
