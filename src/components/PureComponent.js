f0265d229f54f6566f0ec9428b8426c309c3b3ba
Michael Hahn <mwhahn@gmail.com>
2015-09-14 20:22:47 -0700

    Properly set shouldComponentUpdate on the prototype

--- src/components/PureComponent.js
+++ src/components/PureComponent.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import shouldPureComponentUpdate from 'react-pure-render/function';
 
-class PureComponent extends Component {
-    shouldComponentUpdate = shouldPureComponentUpdate;
-}
+class PureComponent extends Component {}
 
 export default PureComponent;
+PureComponent.prototype.shouldComponentUpdate = shouldPureComponentUpdate;
