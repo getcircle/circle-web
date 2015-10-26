3ee33c294802f4230a6794bc55926d59cd1a7dfb
Ravi Rani <ravirani@gmail.com>
2015-10-26 11:03:30 -0700

    Cleanup profile service calls

--- src/services/profile.js
+++ src/services/profile.js
@@ -2,7 +2,6 @@ import _ from 'lodash';
 import { services } from 'protobufs';
 
 import client from './client';
-import logger from '../utils/logger';
 
 export function getProfile(parameters={}) {
     let key = Object.values(parameters)[0];
@@ -57,96 +56,6 @@ export function getExtendedProfile(profileId) {
 
 export function getInitialsForProfile(profile) {
     return [profile.first_name[0], profile.last_name[0]].map((character) => _.capitalize(character)).join('');
-}
-
-export function getProfilesForTeamId(teamId, nextRequest) {
-    /*eslint-disable camelcase*/
-    let parameters = {
-        team_id: teamId,
-    };
-    /*eslint-enable camelcase*/
-
-    // TODO do something with nextRequest
-
-    let request = new services.profile.actions.get_profiles.RequestV1(parameters);
-    return new Promise((resolve, reject) => {
-        client.sendRequest(request)
-            .then((response) => {
-                let { profiles } = response.result;
-                resolve({
-                    profiles,
-                    teamId,
-                    nextRequest: response.getNextRequest(),
-                });
-            })
-            .catch((error) => {
-                logger.log(`Error fetching profliles for team: ${error}`);
-            });
-    });
-}
-
-export function getProfilesForLocationId(locationId, nextRequest) {
-    let parameters = {
-        'location_id': locationId,
-    };
-
-    // TODO do something with nextRequest
-    let request = new services.profile.actions.get_profiles.RequestV1(parameters);
-    return new Promise((resolve, reject) => {
-        client.sendRequest(request)
-            .then((response) => {
-                let { profiles } = response.result;
-                resolve({
-                    profiles,
-                    locationId,
-                    nextRequest: response.getNextRequest(),
-                });
-            })
-            .catch((error) => {
-                logger.error(`Error fetching profiles for location: ${error}`);
-            });
-    });
-}
-
-export function getProfilesForTagId(tagId, nextRequest) {
-    let parameters = {
-        'tag_id': tagId,
-    };
-
-    // TODO do something with nextRequest
-    let request = new services.profile.actions.get_profiles.RequestV1(parameters);
-    return new Promise((resolve, reject) => {
-        client.sendRequest(request)
-            .then((response) => {
-                let { profiles } = response.result;
-                resolve({
-                    profiles,
-                    tagId,
-                    nextRequest: response.getNextRequest(),
-                });
-            })
-            .catch((error) => {
-                logger.error(`Error fetching profiles for tag: ${error}`);
-            });
-    });
-}
-
-export function getTag(tagId) {
-    let parameters = {
-        ids: [tagId],
-    };
-
-    let request = new services.profile.actions.get_tags.RequestV1(parameters);
-    return new Promise((resolve, reject) => {
-        client.sendRequest(request)
-            .then((response) => {
-                let { tags } = response.result;
-                resolve(tags[0]);
-            })
-            .catch((error) => {
-                logger.error(`Error fetching tag: ${error}`);
-            });
-    });
 }
 
 export function updateProfile(profile) {
