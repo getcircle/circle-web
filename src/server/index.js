087cbd825f67580adfb67d252f63ed1ff567e88e
Michael Hahn <mwhahn@gmail.com>
2015-11-30 17:34:19 -0800

    Create a main routes file so we can cleanly capture any errors

--- src/server/index.js
+++ src/server/index.js
@@ -1,18 +1,10 @@
 import Express from 'express';
-import React from 'react';
-import ReactDOM from 'react-dom/server';
-import path from 'path';
 import PrettyError from 'pretty-error';
 import http from 'http';
 import httpProxy from 'http-proxy';
 
-import Client from '../common/services/client';
-import createStore from '../common/createStore';
-import getRoutes from '../common/getRoutes';
-import { Provider } from 'react-redux';
-import { match, RoutingContext } from 'react-router';
-import renderFullPage from './renderFullPage';
-import Root from '../common/Root';
+import main from './routes/main';
+
 
 const pretty = new PrettyError();
 const app = new Express();
@@ -24,66 +16,20 @@ if (__LOCAL__) {
         target: process.env.REMOTE_API_ENDPOINT,
     });
     app.use('/api', (req, res) => {
-        proxy.web(req, res);
+        try {
+            proxy.web(req, res);
+        } catch (e) {
+            console.error('ERROR PROXING API:', pretty.render(e));
+        }
     });
 }
 
 app.use((req, res) => {
-    if (__LOCAL__) {
-        // Do not cache webpack stats: the script file would change since
-        // hot module replacement is enabled in the development env
-        webpackIsomorphicTools.refresh();
+    try {
+        main(req, res);
+    } catch (e) {
+        console.log('ERROR PROCESSING REQUEST:', pretty.render(e));
     }
-
-    global.navigator = {
-        userAgent: req.headers['user-agent'],
-    };
-
-    const client = new Client(req);
-    const store = createStore(client);
-
-    function hydrateOnClient() {
-        res.status(200).send(renderFullPage('', store, webpackIsomorphicTools.assets()));
-    }
-
-    if (__DISABLE_SSR__) {
-        hydrateOnClient();
-        return;
-    }
-
-    match({routes: getRoutes(store), location: req.url}, (error, redirectLocation, renderProps) => {
-        if (error) {
-            console.error('ROUTER ERROR:', pretty.render(error));
-            res.send(500, error.message);
-            hydrateOnClient();
-        } else if (redirectLocation) {
-            res.redirect(302, redirectLocation.pathname + redirectLocation.search);
-        } else if (renderProps) {
-            let content;
-            try {
-                content = ReactDOM.renderToString(
-                    <Root>
-                        <Provider key="provider" store={store}>
-                            <RoutingContext {...renderProps} />
-                        </Provider>
-                    </Root>
-                );
-            } catch (e) {
-                console.error('REACT RENDER ERROR:', pretty.render(e));
-                hydrateOnClient();
-                return;
-            }
-            let page;
-            try {
-                page = renderFullPage(content, store, webpackIsomorphicTools.assets());
-            } catch (e) {
-                console.error('RENDER ERROR:', pretty.render(e));
-                hydrateOnClient();
-                return;
-            }
-            res.status(200).send(page);
-        }
-    });
 });
 
 server.listen(3000, (err) => {
---
+++ src/server/routes/main.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import ReactDOM from 'react-dom/server';
+
+import PrettyError from 'pretty-error';
+import { Provider } from 'react-redux';
+import { match, RoutingContext } from 'react-router';
+
+import Client from '../../common/services/client';
+import createStore from '../../common/createStore';
+import getRoutes from '../../common/getRoutes';
+import Root from '../../common/Root';
+
+import renderFullPage from '../renderFullPage';
+
+const pretty = new PrettyError();
+
+export default function (req, res) {
+    if (__LOCAL__) {
+        // Do not cache webpack stats: the script file would change since
+        // hot module replacement is enabled in the development env
+        webpackIsomorphicTools.refresh();
+    }
+
+    global.navigator = {
+        userAgent: req.headers['user-agent'],
+    };
+
+    const client = new Client(req);
+    const store = createStore(client);
+
+    function hydrateOnClient() {
+        res.status(200).send(renderFullPage('', store, webpackIsomorphicTools.assets()));
+    }
+
+    if (__DISABLE_SSR__) {
+        hydrateOnClient();
+        return;
+    }
+
+    match({routes: getRoutes(store), location: req.url}, (error, redirectLocation, renderProps) => {
+        if (error) {
+            console.error('ROUTER ERROR:', pretty.render(error));
+            res.send(500, error.message);
+            hydrateOnClient();
+        } else if (redirectLocation) {
+            res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+        } else if (renderProps) {
+            let content;
+            try {
+                content = ReactDOM.renderToString(
+                    <Root>
+                        <Provider key="provider" store={store}>
+                            <RoutingContext {...renderProps} />
+                        </Provider>
+                    </Root>
+                );
+            } catch (e) {
+                console.error('REACT RENDER ERROR:', pretty.render(e));
+                hydrateOnClient();
+                return;
+            }
+            let page;
+            try {
+                page = renderFullPage(content, store, webpackIsomorphicTools.assets());
+            } catch (e) {
+                console.error('RENDER ERROR:', pretty.render(e));
+                hydrateOnClient();
+                return;
+            }
+            res.status(200).send(page);
+        }
+    });
+}
