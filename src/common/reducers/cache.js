f9c7d44778b53edb33e9e9b79599db53b824189b
Felix Mo <felix@felixmo.com>
2015-12-04 19:47:29 -0500

    Use arrow function syntax

--- src/common/reducers/cache.js
+++ src/common/reducers/cache.js
@@ -19,8 +19,8 @@ export default function cache(state = initialState, action) {
             // the old normalizations may contain fields that don't exist in the new normalizations
             // and these will remain if merged
             if (action.payload.normalizations) {
-                Immutable.Map(action.payload.normalizations).map(function (normalizations, normalizationsType) {
-                    Immutable.Map(normalizations).map(function (normalization, normalizationId) {
+                Immutable.Map(action.payload.normalizations).map((normalizations, normalizationsType) => {
+                    Immutable.Map(normalizations).map((normalization, normalizationId) => {
                         map.deleteIn(['normalizations', normalizationsType, normalizationId]);
                     });
                 });
