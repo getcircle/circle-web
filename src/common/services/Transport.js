d737b634d900bfef955936f006ac45599c7ec730
Michael Hahn <mwhahn@gmail.com>
2015-12-17 11:00:17 -0800

    Log the access info we're using to fetch the cookie

--- src/common/services/Transport.js
+++ src/common/services/Transport.js
@@ -107,9 +107,10 @@ export default class Transport {
                         const accessInfo = CookieAccessInfo(
                             process.env.AUTHENTICATION_TOKEN_COOKIE_DOMAIN,
                             undefined,
-                            false,
+                            process.env.NODE_ENV === 'production',
                             false,
                         );
+                        console.log('COOKIE ACCESS INFO: %s', JSON.stringify(accessInfo));
                         const cookie = this.agent.jar.getCookies(accessInfo);
                         const allCookies = this.agent.jar.getCookies();
                         console.log('COOKIE: %s', cookie.toString());
