3a66d09822518e18852ff9a80adeedcd3a076348
Michael Hahn <mwhahn@gmail.com>
2016-02-03 17:10:54 -0800

    Remove the old blur component

--- src/common/components/DetailHeader.js
+++ src/common/components/DetailHeader.js
@@ -1,65 +1,19 @@
 import React, { PropTypes } from 'react';
 
-import Blur from './Blur';
-import CSSComponent from './CSSComponent';
-import InternalPropTypes from './InternalPropTypes';
-
-class DetailHeader extends CSSComponent {
-
-    static propTypes = {
-        children: PropTypes.node,
-        img: PropTypes.string,
-        style: PropTypes.object,
+const styles = {
+    root: {
+        backgroundColor: 'rgb(51, 51, 51)',
+        height: 290,
+        position: 'relative',
+        zIndex: 1,
     }
+};
 
-    static contextTypes = {
-        device: InternalPropTypes.DeviceContext.isRequired,
-    }
-
-    classes() {
-        return {
-            default: {
-                root: {
-                    backgroundColor: 'rgb(51, 51, 51)',
-                    height: 290,
-                    position: 'relative',
-                    zIndex: 1,
-                },
-                nonBlurHeader: {
-                    backgroundColor: 'rgb(51, 51, 51)',
-                    height: 290,
-                    position: 'relative',
-                },
-            },
-        };
-    }
-
-    render() {
-        const {
-            img,
-            style,
-            ...other,
-        } = this.props;
-        const { largerDevice } = this.context.device;
-
-        const headerImage = img && img !== '' ? img : '';
-        if (headerImage) {
-            return (
-                <Blur
-                    blurRadius={50}
-                    className={largerDevice ? 'detail-blur-canvas' : 'detail-blur-canvas-notop'}
-                    img={headerImage}
-                >
-                    <header {...other} style={{...this.styles().root, ...style}} />
-                </Blur>
-            );
-        } else {
-            return (
-                <header {...other} style={{...this.styles().nonBlurHeader, ...style}} />
-            );
-        }
-    }
-
+const DetailHeader = ({style, ...other}) => {
+    return <header {...other} style={{...styles.root, ...style}} />;
+};
+DetailHeader.propTypes = {
+    style: PropTypes.object,
 }
 
 export default DetailHeader;
