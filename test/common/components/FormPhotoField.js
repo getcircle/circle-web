13b253722ca1cfa3a0f77c50d47bb61e7ce4216f
Bruz Marzolf <bruz@bruzilla.com>
2016-02-05 11:21:29 -0800

    Add tests for `FormPhotoField`

---
+++ test/common/components/FormPhotoField.js
@@ -0,0 +1,43 @@
+import Dropzone from 'react-dropzone';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import * as formBehaviors from './formBehaviors';
+
+import FormPhotoField from '../../../src/common/components/FormPhotoField';
+
+function setup(overrides) {
+    const defaults = {
+        invalid: true,
+        placeholder: 'A bunch of text',
+        touched: false,
+        value: '',
+    };
+    const props = Object.assign({}, defaults, overrides);
+
+    const wrapper = shallow(<FormPhotoField {...props} />);
+
+    return {
+        props,
+        wrapper,
+    };
+}
+
+describe('FormPhotoField', () => {
+
+    context('when valid', () => {
+        const { wrapper } = setup();
+        formBehaviors.itDoesNotShowError(wrapper.find(Dropzone));
+    });
+
+    context('when invalid but not touched', () => {
+        const { wrapper } = setup({invalid: true, touched: false});
+        formBehaviors.itDoesNotShowError(wrapper.find(Dropzone));
+    });
+
+    context('when invalid and touched', () => {
+        const { wrapper } = setup({invalid: true, touched: true});
+        formBehaviors.itShowsError(wrapper.find(Dropzone));
+    });
+
+});
