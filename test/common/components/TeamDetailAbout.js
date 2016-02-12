bc092654538cbe87268e4cf09ae5fc128eef6fdc
Bruz Marzolf <bruz@bruzilla.com>
2016-02-12 11:55:30 -0800

    Add tests for edit button on team about page

--- test/common/components/TeamDetailAbout.js
+++ test/common/components/TeamDetailAbout.js
@@ -3,9 +3,11 @@ import { shallow } from 'enzyme';
 import React from 'react';
 
 import { getCustomTheme } from '../../../src/common/styles/theme';
+import { showTeamEditModal } from '../../../src/common/actions/teams';
 
 import DetailSection from '../../../src/common/components/DetailSectionV2';
 import DetailListProfiles from '../../../src/common/components/DetailListProfiles';
+import EditIcon from '../../../src/common/components/EditIcon';
 import TeamDetailAbout from '../../../src/common/components/TeamDetailAbout';
 
 import TeamFactory from '../../factories/TeamFactory';
@@ -34,7 +36,29 @@ describe('TeamDetailAbout', () => {
 
     it('renders the "About" heading', () => {
         const { wrapper } = setup();
-        expect(wrapper.find('h1').text()).toEqual('About');
+        expect(wrapper.find('h1').text()).toEqual('About <EditIcon />');
+    });
+
+    describe('edit button', () => {
+
+        it('is shown when the user has edit permissions', () => {
+            const { wrapper } = setup({}, {noPermissions: false});
+            expect(wrapper.find(EditIcon).length).toBe(1);
+        });
+
+        it('is not shown when the user does not have edit permissions', () => {
+            const { wrapper } = setup({}, {noPermissions: true});
+            expect(wrapper.find(EditIcon).length).toBe(0);
+        });
+
+        it('onTouchTap dispatches the show modal action', () => {
+            const dispatchSpy = expect.createSpy();
+            const { wrapper } = setup({dispatch: dispatchSpy});
+            wrapper.find(EditIcon).prop('onTouchTap')();
+            const action = showTeamEditModal();
+            expect(dispatchSpy).toHaveBeenCalledWith(action);
+        });
+
     });
 
     describe('description section', () => {
