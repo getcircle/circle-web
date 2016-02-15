b40c71e55e0e4ec9b99368bd23a043c1d8866d00
Bruz Marzolf <bruz@bruzilla.com>
2016-02-15 11:05:21 -0800

    Propagate `dispatch` through team components where missing

--- src/common/components/DetailListProfiles.js
+++ src/common/components/DetailListProfiles.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { services } from 'protobufs';
 
 import { List } from 'material-ui';
 
 import DetailListItemProfile from './DetailListItemProfile';
 
-const DetailListProfiles = ({ profiles }) => {
-    const items = profiles.map((p, i) => <DetailListItemProfile key={`detail-list-item-profile-${i}`} profile={p} />);
+const DetailListProfiles = ({ dispatch, profiles }) => {
+    const items = profiles.map((p, i) => {
+        return (
+            <DetailListItemProfile
+                dispatch={dispatch}
+                key={`detail-list-item-profile-${i}`}
+                profile={p}
+            />
+        );
+    });
     return <List children={items} />;
+};
+DetailListProfiles.propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    profile: PropTypes.arrayOf(
+        PropTypes.instanceOf(services.profile.containers.ProfileV1)
+    ),
 };
 
 export default DetailListProfiles;
