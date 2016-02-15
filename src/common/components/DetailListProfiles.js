fd6ee486072657e9c9678a6424b7b1cb1f4057ba
Bruz Marzolf <bruz@bruzilla.com>
2016-02-15 13:42:00 -0800

    Revert unneeded changes to DetailListProfiles

--- src/common/components/DetailListProfiles.js
+++ src/common/components/DetailListProfiles.js
@@ -1,25 +1,12 @@
 import React, { PropTypes } from 'react';
-import { services } from 'protobufs';
 
 import { List } from 'material-ui';
 
 import DetailListItemProfile from './DetailListItemProfile';
 
 const DetailListProfiles = ({ profiles }) => {
-    const items = profiles.map((p, i) => {
-        return (
-            <DetailListItemProfile
-                key={`detail-list-item-profile-${i}`}
-                profile={p}
-            />
-        );
-    });
+    const items = profiles.map((p, i) => <DetailListItemProfile key={`detail-list-item-profile-${i}`} profile={p} />);
     return <List children={items} />;
-};
-DetailListProfiles.propTypes = {
-    profile: PropTypes.arrayOf(
-        PropTypes.instanceOf(services.profile.containers.ProfileV1)
-    ),
 };
 
 export default DetailListProfiles;
