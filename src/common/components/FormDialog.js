cff0d8fa05af24bad26d1b470ff307da45cb3c93
Michael Hahn <mwhahn@gmail.com>
2016-02-15 18:09:18 -0800

    Remove extra space

--- src/common/components/FormDialog.js
+++ src/common/components/FormDialog.js
@@ -105,7 +105,7 @@ export default class FormDialog extends CSSComponent {
         };
 
         return (
-            <div >
+            <div>
                 <Dialog
                     bodyStyle={styles.body}
                     dialogDismissLabel={t('Cancel')}
