c265357045a7e8f26baa6c7c2dbb9d67b518b565
Bruz Marzolf <bruz@bruzilla.com>
2016-02-16 11:58:24 -0800

    Extract generic IconMenu from MoreMenu

---
+++ src/common/components/IconMenu.js
@@ -0,0 +1,57 @@
+import { IconButton, IconMenu as MaterialIconMenu } from 'material-ui';
+import merge from 'merge';
+import React, { Component, PropTypes } from 'react';
+
+export default class IconMenu extends Component {
+
+    static propTypes = {
+        children: PropTypes.node,
+        hover: PropTypes.bool,
+        iconElement: PropTypes.func.isRequired,
+        style: PropTypes.object,
+    }
+
+    static defaultProps = {
+        style: {},
+    }
+
+    state = {
+        open: false,
+    }
+
+    handleRequestChange = (open) => {
+        this.setState({open: open});
+    }
+
+    render() {
+        const { children, hover, iconElement, style } = this.props;
+        const { open } = this.state;
+        const styles = {
+            button: {
+                left: 6,
+                position: 'absolute',
+                top: 6,
+            },
+            menu: {},
+            root: {},
+        };
+        if (!hover && !open) {
+            styles.root.display = 'none';
+            styles.menu.display = 'none';
+        }
+        const button = <IconButton>{iconElement}</IconButton>;
+        return (
+            <MaterialIconMenu
+                anchorOrigin={{horizontal: 'middle', vertical: 'bottom'}}
+                iconButtonElement={button}
+                menuStyle={styles.menu}
+                onRequestChange={this.handleRequestChange}
+                open={this.state.open}
+                style={merge(styles.root, style)}
+                targetOrigin={{horizontal: 'middle', vertical: 'top'}}
+            >
+                {children}
+            </MaterialIconMenu>
+        );
+    }
+}
--- src/common/components/MoreMenu.js
+++ src/common/components/MoreMenu.js
@@ -1,83 +1,58 @@
-import React, { Component, PropTypes } from 'react';
-import IconMenu from 'material-ui/lib/menus/icon-menu';
-import IconButton from 'material-ui/lib/icon-button';
-import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
+import React, { PropTypes } from 'react';
 
+import IconMenu from './IconMenu';
 import MoreIcon from './MoreIcon';
 
-export default class MoreMenu extends Component {
+const MoreMenu = ({children, hover}, {muiTheme}) => {
+    const { tintColor } = muiTheme.luno;
+    const styles = {
+        button: {
+            left: 6,
+            position: 'absolute',
+            top: 6,
+        },
+        circle: {
+            border: `2px solid ${tintColor}`,
+            borderRadius: '50%',
+            left: 12,
+            height: 24,
+            position: 'absolute',
+            top: 12,
+            width: 24,
+        },
+        root: {
+            position: 'absolute',
+            right: 18,
+            top: 15,
+        },
+    };
+    const icon = (
+        <div>
+            <MoreIcon
+                height={36}
+                stroke={tintColor}
+                style={styles.button}
+                width={36}
+            />
+            <div style={styles.circle} />
+        </div>
+    );
+    return (
+        <IconMenu
+            hover={hover}
+            iconElement={icon}
+            style={styles.root}
+        >
+            {children}
+        </IconMenu>
+    );
+};
+MoreMenu.propTypes = {
+    children: PropTypes.node,
+    hover: PropTypes.bool,
+};
+MoreMenu.contextTypes = {
+    muiTheme: PropTypes.object.isRequired,
+};
 
-    static propTypes = {
-        children: PropTypes.node,
-        hover: PropTypes.bool,
-    }
-
-    static contextTypes = {
-        muiTheme: PropTypes.object.isRequired,
-    }
-
-    state = {
-        open: false,
-    }
-
-    handleRequestChange = (open) => {
-        this.setState({open: open});
-    }
-
-    render() {
-        const { children, hover } = this.props;
-        const { tintColor } = this.context.muiTheme.luno;
-        const { open } = this.state;
-        const styles = {
-            button: {
-                left: 6,
-                position: 'absolute',
-                top: 6,
-            },
-            circle: {
-                border: `2px solid ${tintColor}`,
-                borderRadius: '50%',
-                left: 12,
-                height: 24,
-                position: 'absolute',
-                top: 12,
-                width: 24,
-            },
-            menu: {},
-            root: {
-                position: 'absolute',
-                right: 18,
-                top: 15,
-            },
-        };
-        if (!hover && !open) {
-            styles.root.display = 'none';
-            styles.menu.display = 'none';
-        }
-        const button = (
-            <IconButton>
-                <MoreIcon
-                    height={36}
-                    stroke={tintColor}
-                    style={styles.button}
-                    width={36}
-                />
-                <div style={styles.circle} />
-            </IconButton>
-        );
-        return (
-            <IconMenu
-                anchorOrigin={{horizontal: 'middle', vertical: 'bottom'}}
-                iconButtonElement={button}
-                iconStyle={styles.icon}
-                menuStyle={styles.menu}
-                onRequestChange={this.handleRequestChange}
-                open={this.state.open}
-                style={styles.root}
-                targetOrigin={{horizontal: 'middle', vertical: 'top'}}
-            >
-                {children}
-            </IconMenu>
-        );
-    }
-}
+export default MoreMenu;
