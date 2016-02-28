09dce1a11792674268fd19df48c4d670330ee669
Michael Hahn <mwhahn@gmail.com>
2016-02-28 11:46:21 -0800

    Handle case where user isn't a member

--- src/common/reducers/teamMembership.js
+++ src/common/reducers/teamMembership.js
@@ -13,7 +13,10 @@ export default function (state = initialState, action) {
     const { type, payload } = action;
     switch(type) {
     case types.GET_TEAM_SUCCESS:
-        return state.setIn([payload.result, 'memberId'], payload.member.id);
+        if (payload.member) {
+            return state.setIn([payload.result, 'memberId'], payload.member.id);
+        }
+        break;
     case types.JOIN_TEAM_SUCCESS:
         const memberId = getTeamMemberNormalizationFromJoinTeam(action);
         return state.setIn([payload.result, 'memberId'], memberId);
