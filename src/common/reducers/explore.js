80a4b1b9eaf8bf075f81f464e016d05e1a8b2cf5
Michael Hahn <mwhahn@gmail.com>
2016-02-29 12:16:32 -0800

    Keep explore in sync with client side changes

--- src/common/reducers/explore.js
+++ src/common/reducers/explore.js
@@ -8,10 +8,16 @@ import paginate from './paginate';
 import * as types from '../constants/actionTypes';
 
 function additionalTypesCallback(state, action) {
+    let post;
     switch (action.type) {
     case types.DELETE_POST_SUCCESS:
-        const { post } = action.payload;
+        post = action.payload.post;
         return state.updateIn([EXPLORE_TYPES.POSTS, 'ids'], set => set ? set.delete(post.id) : set);
+    case types.CREATE_POST_SUCCESS:
+        post = action.payload.post;
+        return state.updateIn([EXPLORE_TYPES.POSTS, 'ids'], set => set ? set.add(post.id) : set);
+    case types.CREATE_TEAM_SUCCESS:
+        return state.updateIn([EXPLORE_TYPES.TEAMS, 'ids'], set => set ? set.add(action.payload.result) : set);
     }
     return state;
 }
