93401ce3d056c902c9be2f436cae0b9bd4779066
Bruz Marzolf <bruz@bruzilla.com>
2016-03-02 15:14:46 -0800

    Add a confirmation message in add to collection menu

--- src/common/components/AddToCollectionMenu.js
+++ src/common/components/AddToCollectionMenu.js
@@ -3,6 +3,7 @@ import React, { Component, PropTypes } from 'react';
 import { services } from 'protobufs';
 import { getValues } from 'redux-form';
 
+import t from '../utils/gettext';
 import { updateCollections } from '../utils/collections';
 
 import AddCollectionIcon from './AddCollectionIcon';
@@ -10,11 +11,19 @@ import AddToCollectionForm from './AddToCollectionForm';
 import IconMenu from './IconMenu';
 
 const FILTER_INPUT_CLASS_NAME = 'add-to-collection-input';
+const CONFIRMATION_CLOSE_DELAY = 3000;
+
+function missingCollection(collections, otherCollections) {
+    return collections.find(collection => {
+        return !otherCollections.find(o => o.id === collection.id);
+    });
+}
 
 class AddToCollectionMenu extends Component {
 
     state = {
         addingNewCollection: false,
+        confirmationMessage: null,
         muiTheme: this.context.muiTheme,
         open: false,
     }
@@ -31,10 +40,46 @@ class AddToCollectionMenu extends Component {
         this.setState({muiTheme});
     }
 
+    componentWillReceiveProps(nextProps) {
+        const oldCollections = this.props.collections;
+        const newCollections = nextProps.collections;
+        if (!oldCollections || !newCollections) {
+            return;
+        }
+
+        const added = missingCollection(newCollections, oldCollections);
+        const removed = missingCollection(oldCollections, newCollections);
+
+        let confirmationMessage;
+        if (added) {
+            confirmationMessage = t('Added to ') + added.display_name;
+        } else if (removed) {
+            confirmationMessage = t('Removed from ') + removed.display_name;
+        }
+
+        if (confirmationMessage) {
+            this.setState({confirmationMessage});
+
+            this.confirmationTimeout = setTimeout(() => {
+                this.setState({open: false});
+            }, CONFIRMATION_CLOSE_DELAY);
+        }
+    }
+
     handleRequestChange = (open) => {
-        this.setState({addingNewCollection: false, open});
-        if (!open) {
+        if (open) {
+            this.setState({
+                addingNewCollection: false,
+                confirmationMessage: null,
+                open
+            });
+        } else {
+            this.setState({open});
             this.submit();
+        }
+
+        if (this.confirmationTimeout) {
+            clearTimeout(this.confirmationTimeout);
         }
     }
 
@@ -55,17 +100,16 @@ class AddToCollectionMenu extends Component {
         form = getValues(this.context.store.getState().get('form').addToCollection);
         const { collections, post } = this.props;
         updateCollections(dispatch, post, collections, form.collections);
-        this.setState({addingNewCollection: false, open: false});
     }
 
     handleItemTouchTap = (event) => {
+        event.preventDefault();
+
         const inputTarget = event.target.className === FILTER_INPUT_CLASS_NAME;
         if (inputTarget || this.state.addingNewCollection) {
-            event.preventDefault();
             return;
         }
 
-        this.setState({open: false, addingNewCollection: false});
         this.submit();
     }
 
@@ -78,9 +122,13 @@ class AddToCollectionMenu extends Component {
     render() {
         const { collections, editableCollections, memberships, post, style, ...other } = this.props;
         const { muiTheme } = this.context;
-        const { addingNewCollection } = this.state;
+        const { addingNewCollection, confirmationMessage } = this.state;
 
         const styles = {
+            confirmation: {
+                fontSize: '1.5rem',
+                padding: '5px 15px',
+            },
             formContainer: {
                 padding: 20,
                 paddingTop: 0,
@@ -99,25 +147,22 @@ class AddToCollectionMenu extends Component {
                 WebkitBoxShadow: 'none',
             },
             menu: {
-                height: addingNewCollection ? undefined : 270,
+                height: addingNewCollection || confirmationMessage ? undefined : 270,
                 width: 340,
             },
         };
 
         const theme = muiTheme.luno.circularIconMenu;
-        return (
-            <IconMenu
-                closeOnItemTouchTap={false}
-                iconButtonStyle={theme.button}
-                iconElement={<AddCollectionIcon {...theme.Icon} />}
-                menuStyle={styles.menu}
-                onItemTouchTap={this.handleItemTouchTap}
-                onRequestChange={this.handleRequestChange}
-                open={this.state.open}
-                style={merge(theme.menu, style)}
-                useLayerForClickAway={true}
-                {...other}
-            >
+
+        let content;
+        if (confirmationMessage) {
+            content = (
+                <div style={styles.confirmation}>
+                    {confirmationMessage}
+                </div>
+            );
+        } else {
+            content = (
                 <div style={styles.formContainer}>
                     <AddToCollectionForm
                         addingNewCollection={addingNewCollection}
@@ -136,6 +181,22 @@ class AddToCollectionMenu extends Component {
                         style={styles.form}
                     />
                 </div>
+            );
+        }
+        return (
+            <IconMenu
+                closeOnItemTouchTap={false}
+                iconButtonStyle={theme.button}
+                iconElement={<AddCollectionIcon {...theme.Icon} />}
+                menuStyle={styles.menu}
+                onItemTouchTap={this.handleItemTouchTap}
+                onRequestChange={this.handleRequestChange}
+                open={this.state.open}
+                style={merge(theme.menu, style)}
+                useLayerForClickAway={true}
+                {...other}
+            >
+                {content}
             </IconMenu>
         );
     }
