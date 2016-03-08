9f3b8565507f7acfde83e9ad01de9b43c20125ea
Michael Hahn <mwhahn@gmail.com>
2016-03-08 07:48:52 -0800

    As soon as we create the team, the user should be considered a member

--- src/common/reducers/teamMembership.js
+++ src/common/reducers/teamMembership.js
@@ -1,17 +1,35 @@
 import Immutable from 'immutable';
+import { services } from 'protobufs';
 
 import {
     getTeamMemberNormalizationFromJoinTeam,
+    getTeamMemberNormalizationsFromCreateTeam,
 } from './normalizations';
+import { retrieveTeamMembers } from './denormalizations';
 import * as types from '../constants/actionTypes';
 
-const initialState = Immutable.fromJS({
+const { COORDINATOR } = services.team.containers.TeamMemberV1.RoleV1;
 
-});
+const initialState = Immutable.Map();
 
 export default function (state = initialState, action) {
     const { type, payload } = action;
     switch(type) {
+    case types.CREATE_TEAM_SUCCESS:
+        let coordinator;
+        const ids = getTeamMemberNormalizationsFromCreateTeam(action);
+        if (ids) {
+            const members = retrieveTeamMembers(ids, action.payload);
+            for (let member of members) {
+                if (member.role === COORDINATOR) {
+                    coordinator = member;
+                    break;
+                }
+            }
+        }
+        if (coordinator) {
+            return state.setIn([action.payload.result, 'memberId'], coordinator.id);
+        }
     case types.GET_TEAM_SUCCESS:
         if (payload.member) {
             return state.setIn([payload.result, 'memberId'], payload.member.id);
